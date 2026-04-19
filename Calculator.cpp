8ec5c8b6a667f075ff1f17c4c6beda1ea2bac9ab
RomanT1K2 <roman2212c@gmail.com>
2026-04-19 19:25:55 +0300

    first commit

---
+++ Calculator.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+double add(double a, double b) {
+    return a + b;
+}
+
+double subtract(double a, double b) {
+    return a - b;
+}
+
+int main() {
+    double a, b;
+    char op;
+
+    cout << "Enter expression (a + b): ";
+    cin >> a >> op >> b;
+
+    if (op == '+') {
+        cout << "Result: " << add(a, b);
+    }
+    else if (op == '-') {
+        cout << "Result: " << subtract(a, b);
+    }
+    else {
+        cout << "Unsupported operation";
+    }
+
+    return 0;
+}
