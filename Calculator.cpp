88c32ff9f1817cc6084a2ed475b9c49a134a720e
RomanT1K2 <roman2212c@gmail.com>
2026-04-19 22:30:05 +0300

    Refactor Calculator class with basic operations
    Refactored Calculator class to include methods for addition, subtraction, multiplication, and division, but currently returns 0 for each operation.

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,44 +1,21 @@
 #include <iostream>
 using namespace std;
 
-double add(double a, double b) {
-    return a + b;
-}
-
-double subtract(double a, double b) {
-    return a - b;
-}
-double multiply(double a, double b) {
-    return a * b;
-}
-
-double divide(double a, double b) {
-    return a / b;
-}
-
-int main() {
-    double a, b;
-    char mark;
-
-    cout << "Enter expression (a...b): ";
-    cin >> a >> mark >> b;
- 
-	if (mark == '+') {
-        cout << "Result: " << add(a, b);
-    }
-    else if (mark == '-') {
-        cout << "Result: " << subtract(a, b);
-    }
-    else if (mark == '*') {
-        cout << "Result: " << multiply(a, b);
-    }
-    else if (mark == '/') {
-        cout << "Result: " << divide(a, b);
-    }
-    else {
-        cout << "Unsupported operation";
+class Calculator {
+public:
+    double add(double a, double b) {
+        return 0;
     }
 
-    
-    return 0;
-}
+    double subtract(double a, double b) {
+        return 0;
+    }
+
+    double multiply(double a, double b) {
+        return 0; 
+    }
+
+    double divide(double a, double b) {
+        return 0; 
+    }
+};
